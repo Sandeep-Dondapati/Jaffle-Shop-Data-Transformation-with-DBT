034287bdfccbbb5c818b8e23361eeee7e8a3d712
Sandeep Dondapati <sandeepdpractice@gmail.com>
2023-11-02 05:26:20 +0000

    updated fct_orders and dim_customers sql file

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,9 +1,9 @@
 SELECT 
     id as payment_id,
     orderid as order_id,
-    paymentmethod,
-    amount,
+    paymentmethod as payment_method,
+    amount/100 as amount,
     status,
-    created as created_date
+    created as created_at
 
-FROM `dbt-learn-bigquery-403813.stripe.payments` 
+FROM dbt-learn-bigquery-403813.stripe.payments 
